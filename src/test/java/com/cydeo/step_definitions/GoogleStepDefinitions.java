72479e4f4a3c432e58300252464fe0ebeefed585
Burockjohn <brkcn87@gmail.com>
2023-08-02 12:37:45 +0300

    day 16 - parameterization

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -9,19 +9,32 @@ import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
-    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
-
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    @When("user types {word} and clicks enter")
+    public void user_types_and_clicks_enter2(String string) {
+        googleSearchPage.searchBox.sendKeys(string + Keys.ENTER);
     }
 
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-        String expectedTitle = "apple - Google'da Ara";
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String string) {
+        googleSearchPage.searchBox.sendKeys(string + Keys.ENTER);
+    }
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+        String expectedTitle = string + " - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
+    }
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String anything) {
+        String expectedTitle = anything+" - Google'da Ara";
         String actualTitle = Driver.getDriver().getTitle();
         //Junit assertion accepts first arg as expected, second arg as actual
-        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
     }
 
     @When("user is on Google search page")
