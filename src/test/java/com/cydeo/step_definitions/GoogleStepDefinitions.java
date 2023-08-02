5c3382f1c9b31218224e3eb8578ae4a73f75556e
Burockjohn <brkcn87@gmail.com>
2023-08-02 12:06:55 +0300

    GoogleStepDefinitions update

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -36,7 +36,5 @@ public class GoogleStepDefinitions {
         String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle, expectedTitle);
-
-        Driver.closeDriver();
     }
 }
