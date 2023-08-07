233ee724f2348d2109d3311cb97ddc549226b70a
Burockjohn <brkcn87@gmail.com>
2023-08-07 11:58:14 +0300

    day 17 - Parameterization and DataTables review

--- src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -41,6 +41,6 @@ public class WebTable_StepDefinitions {
 
     @When("user enters username {string} password {string} and logins")
     public void userEntersUsernamePasswordAndLogins(String username, String password) {
-        webTableLoginPage.login(username, password);
+        webTableLoginPage.login(username,password);
     }
 }
