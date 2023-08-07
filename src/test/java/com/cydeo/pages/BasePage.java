68d2f4bfda2a01a39d11691c25092876b56135d3
Burockjohn <brkcn87@gmail.com>
2023-08-07 13:12:24 +0300

    day 17 - BasePage

---
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -0,0 +1,23 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilites.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//in this class we will store Web elements common to all pages
+public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//button[.='View all orders']")
+    public WebElement viewAllOrders;
+
+    @FindBy(xpath = "//button[.='View all products']")
+    public WebElement viewAllProducts;
+
+    @FindBy(xpath = "//button[.='Order']")
+    public WebElement order;
+}
