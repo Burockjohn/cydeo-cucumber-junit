8fd7cc0473a944a5d0a93e89c85461c9bcb92a2f
Burockjohn <brkcn87@gmail.com>
2023-08-08 11:50:17 +0300

    day 19 - pretty plugin and publish = true

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,13 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "pretty",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@wip",
+        publish = true
 )
 public class CukesRunner {
 }
